b7b7315105a5db16d1c290043e1517574f68f8f8
cool-changelog-3932 <ruizj3@uw.edu>
2019-10-13 23:58:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,7 @@ end
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
   new_hash = {}
-  new_hash[key]=value
+  new_hash = {key:value}
 end
 
 def read_from_hash(hash, key)
